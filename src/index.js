2b011f49387b9bb251409261715f204e3fde6f75
Andi Suryo Laksono <135946860+Andisrylsn@users.noreply.github.com>
2023-07-20 02:30:57 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
